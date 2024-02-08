e16a5d0499ceea451256e77e99fa0a6105883b63
N-Jia-Wen <157371796+N-Jia-Wen@users.noreply.github.com>
2024-02-08 21:52:31 +0800

    Update main.py
    Added code to handle KeyError in case user enters characters that are not letters (uses try, except, and else keywords).

--- Day 26 Project - NATO Phonetic Alphabet Generator/NATO-alphabet-start/main.py
+++ Day 26 Project - NATO Phonetic Alphabet Generator/NATO-alphabet-start/main.py
@@ -1,8 +1,15 @@
 import pandas
 
+generate_alphabet = True
 data_frame = pandas.read_csv("nato_phonetic_alphabet.csv")
 alphabet_dict = {row.letter: row.code for (index, row) in data_frame.iterrows()}
 
-input_text = input("Enter a word: ").upper()
-output_list = [alphabet_dict[letter] for letter in input_text]
-print(output_list)
+while generate_phonetic is True:
+    try:
+        input_text = input("Enter a word: ").upper()
+        output_list = [alphabet_dict[letter] for letter in input_text]
+        print(output_list)
+    except KeyError:
+        print("Please only input letters in the alphabet please.")
+    else:
+        generate_phonetic = False
